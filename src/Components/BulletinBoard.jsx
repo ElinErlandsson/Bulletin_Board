0e20763170f29b33b85cf56ca99a8a69102a93cb
Elin Erlandsson <elin.erlandsson@utb.ecutbildning.se>
2021-03-16 10:59:07 +0100

    componentsCreated

---
+++ src/Components/BulletinBoard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactPost from './ReactPost';
+
+export default function BulletinBoard() {
+    return (
+        <div>
+            <ReactPost/>
+        </div>
+    )
+}
---
+++ src/Components/ReactPost.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PostLike from './PostLike';
+
+export default function ReactPost() {
+    return (
+        <div>
+           <PostLike/> 
+        </div>
+    )
+}
